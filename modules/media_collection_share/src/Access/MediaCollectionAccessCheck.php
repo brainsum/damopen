788fecf27506c479f41a8969970ac9fefa16dacf
Levente Besenyei <lbesenyei@brainsum.com>
2024-06-17 16:35:22 +0200

    DAMO-246 | Removed wrong file.

--- modules/media_collection_share/src/Access/MediaCollectionAccessCheck.php
+++ modules/media_collection_share/src/Access/MediaCollectionAccessCheck.php
@@ -8,14 +8,31 @@ use Drupal\Core\Routing\Access\AccessInterface;
 use Drupal\Core\Session\AccountInterface;
 use Symfony\Component\Routing\Route;
 
+/**
+ * Provides an access checker for media collections.
+ */
 class MediaCollectionAccessCheck implements AccessInterface {
 
+  /**
+   * The entity type manager.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
   protected $entityTypeManager;
 
+  /**
+   * Constructor.
+   *
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
+   *   The entity type manager.
+   */
   public function __construct(EntityTypeManagerInterface $entity_type_manager) {
     $this->entityTypeManager = $entity_type_manager;
   }
 
+  /**
+   * {@inheritdoc}
+   */
   public function access(AccountInterface $account, Route $route, $media_collection) {
     $collection = $this->entityTypeManager->getStorage('media_collection')->load($media_collection);
     if ($collection && (
