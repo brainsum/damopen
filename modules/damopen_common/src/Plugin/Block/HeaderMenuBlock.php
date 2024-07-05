15b2e4c4c202e269daa364370e580bcb68b09adb
Levente Besenyei <lbesenyei@brainsum.com>
2024-07-05 11:56:47 +0200

    Revert "DAMO-340 | Disabled Asset approval menu item for Agencies."
    This reverts commit d1b49a6d540c2f9c422bf3c297f98856ba878c9c.

--- modules/damopen_common/src/Plugin/Block/HeaderMenuBlock.php
+++ modules/damopen_common/src/Plugin/Block/HeaderMenuBlock.php
@@ -63,23 +63,20 @@ class HeaderMenuBlock extends BlockBase {
       ];
     }
 
-    /*
-     * TODO: Review case to view own unpublished assets.
-      if (array_intersect($roles, $view_own_unpublished)) {
-        // Count all unpublished media.
-        $query = \Drupal::entityQuery('media')
-          ->condition('status', 0)
-          ->condition('uid', $current_user->id())
-          ->accessCheck(TRUE);
-        $count = $query->count()->execute();
-        $menu['manage_assets'] = [
-          'title' => new TranslatableMarkup('Assets waiting for approval'),
-          'url' => Url::fromRoute('view.unpublished_assets.user_unpublished_assets')->toString(),
-          'class' => '',
-          'count' => $count,
-        ];
-      }
-    */
+    if (array_intersect($roles, $view_own_unpublished)) {
+      // Count all unpublished media.
+      $query = \Drupal::entityQuery('media')
+        ->condition('status', 0)
+        ->condition('uid', $current_user->id())
+        ->accessCheck(TRUE);
+      $count = $query->count()->execute();
+      $menu['manage_assets'] = [
+        'title' => new TranslatableMarkup('Assets waiting for approval'),
+        'url' => Url::fromRoute('view.unpublished_assets.user_unpublished_assets')->toString(),
+        'class' => '',
+        'count' => $count,
+      ];
+    }
     // Add the user menu.
     $menu['user'] = [
       'view' => [
