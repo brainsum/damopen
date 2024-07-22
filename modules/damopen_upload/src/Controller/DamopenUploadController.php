5a089d65a642276097832464b834d85e7df56ef6
Adam Herczeg <aherczeg@brainsum.com>
2024-07-22 11:49:04 +0200

    DAMO-284 | Controller added.

---
+++ modules/damopen_upload/src/Controller/DamopenUploadController.php
@@ -0,0 +1,26 @@
+<?php
+namespace Drupal\damopen_upload\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+
+/**
+ * Provides route responses for the Example module.
+ */
+class DamopenUploadController extends ControllerBase {
+
+  /**
+   * Returns a simple page.
+   *
+   * @return array
+   *   A simple renderable array.
+   */
+  public function upload() {
+    return [
+      '#theme' => 'damopen_upload_form',
+      '#cache' => [
+        'max-age' => 0,
+      ],
+    ];
+  }
+
+}
